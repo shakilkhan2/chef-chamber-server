57886d3aa2505fc21c5f23b5556c636df745d16d
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-03 19:35:06 +0600

    recipe data added

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const port = process.env.Port || 5000;
 
 const chefs = require("./data/chefs.json");
 
+const recipes = require("./data/recipes.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("chefs' are cooking");
 });
 
+// chef data:
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+// recipe data:
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
